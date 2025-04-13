f28e1ee975443cf55cc76fbb7b3a5de80f09097c
urmoit <89414990+urmoit@users.noreply.github.com>
2025-04-13 19:35:23 +0300

    Update main uncrrypted.lua

--- main uncrrypted.lua
+++ main uncrrypted.lua
@@ -153,7 +153,5 @@ local additionalCredits = creditTab:CreateParagraph({
 - Script contributors.
 - Beta testers for providing feedback.
 - Everyone involved in the development and testing process.
-
-      test
 ]]
 })
