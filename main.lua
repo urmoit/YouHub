6734350513640e11f85d07f46391e232ccf2e529
urmoit <89414990+urmoit@users.noreply.github.com>
2025-05-08 16:47:42 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,17 @@
+-- Grow a Garden
+if game.PlaceId == 126884695634066 then
+    -- Launch the external script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/urmoit/YouHub/refs/heads/main/main%20uncrrypted.lua"))()
+end
+
+-- BGSI
+if game.PlaceId == 85896571713843 then
+    -- Launch the external script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/urmoit/YouHub/refs/heads/main/Games/Grow%20A%20Garden.lua"))()
+end
+
+-- universal
+if game.PlaceId == nil then
+    -- Launch the external script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/urmoit/YouHub/refs/heads/main/main%20uncrrypted.lua"))()
+end
