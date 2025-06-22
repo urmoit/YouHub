e84aca198d7a95667c797a49be5c9f29e0660381
urmoit <89414990+urmoit@users.noreply.github.com>
2025-06-22 16:42:49 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,6 +10,12 @@ if game.PlaceId == 85896571713843 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/urmoit/YouHub/refs/heads/main/Games/Grow%20A%20Garden.lua"))()
 end
 
+-- Build an Island
+if game.PlaceId == 101949297449238 then
+    -- Launch the external script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/urmoit/YouHub/refs/heads/main/Games/Build%20an%20island.lua"))()
+end
+
 -- universal
 if game.PlaceId == nil then
     -- Launch the external script
